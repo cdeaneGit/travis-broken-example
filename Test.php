7e8e33192a0781b9cfd7e4477ba2552be38566b7
Cricket Deane <cdeane@utk.edu>
2017-03-18 12:28:21 -0400

    back to original Test.php

--- Test.php
+++ Test.php
@@ -3,9 +3,7 @@
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
-		/*original bad code
-		$this->assertEquals(1+1,1);*/
-		$this->assertEquals(1+1,2);
+		$this->assertEquals(1+1,1);
   	}
 }
 
